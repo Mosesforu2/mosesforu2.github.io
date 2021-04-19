ab93faf66e709d8c8103833dde3c0fe5dfc14e80
akisupi <63178023+Mosesforu2@users.noreply.github.com>
2021-04-19 17:42:32 +0000

    Updated project
    This is the second update to my first project

--- server.php
+++ server.php
@@ -11,23 +11,23 @@ $link = mysqli_connect('localhost', 'root', 'moSeS1998#', 'proj');
 
 // REGISTER USER
 if (isset($_POST['reg_user'])) {
-  // receive all input values from the form
+  // receive all the data entered by the user from the form
   $username = mysqli_real_escape_string($link, $_POST['username']);
   $email = mysqli_real_escape_string($link, $_POST['email']);
   $password_1 = mysqli_real_escape_string($link, $_POST['password_1']);
   $password_2 = mysqli_real_escape_string($link, $_POST['password_2']);
 
-  // form validation: ensure that the form is correctly filled ...
-  // by adding (array_push()) corresponding error unto $errors array
-  if (empty($username)) { array_push($errors, "Username is required"); }
+  /* form validation: ensuring that the form is filled correctly 
+   by adding (array_push()) corresponding error unto $errors array */
+  if (empty($username)) { array_push($errors, "Please provide a username"); }
   if (empty($email)) { array_push($errors, "Email is required"); }
-  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if (empty($password_1)) { array_push($errors, "You forgot to set your passwoord"); }
   if ($password_1 != $password_2) {
-	array_push($errors, "The two passwords do not match");
+	array_push($errors, "Passwords do not match. Plese check again!");
   }
 
-  // first check the database to make sure 
-  // a user does not already exist with the same username and/or email
+  /* Checking the database to make sure that a user does not already 
+      exist with the same username and/or email*/
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
   $result = mysqli_query($link, $user_check_query);
   $user = mysqli_fetch_assoc($result);
@@ -77,7 +77,7 @@ if (isset($_POST['login_user'])) {
           $_SESSION['success'] = "You are now logged in";
           header('location: index.php');
         }else {
-            array_push($errors, "Wrong username/password combination");
+            array_push($errors, "Username or password is incorrect!");
         }
     }
   }
